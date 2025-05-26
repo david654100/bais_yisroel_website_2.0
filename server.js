6228078feb77f73fcc307c293cd90976c280d181
emmyrunge <michalrunge613@gmail.com>
2025-05-26 00:32:36 -0400

    cors error

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://david654100.github.io"
+}));
+
 app.use(express.json());
 
 const {
